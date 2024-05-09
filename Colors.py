d026eeef4391b7db5df69a885822629d1639f584
Antoniozus89 <166949414+Antoniozus89@users.noreply.github.com>
2024-05-09 14:39:55 +0300

    Add files via upload

---
+++ Colors.py
@@ -0,0 +1,12 @@
+a = 'red'
+b = 'blue'
+c = 'yelow'
+
+if a + b:
+    print('purple')
+elif a + c:
+    print('orange')
+elif b + c:
+    print('gren')
+else:
+    print('Такие цвета смешивать нельзя, получится грязь)')
